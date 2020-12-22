d83287c128d9e130dfe5c453388efe24ae3203e7
lamb572 <55807998+lamb572@users.noreply.github.com>
2020-12-22 19:28:07 +0000

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'b55b73cbae044ad0a8dafdba41d63532'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models.predict({id: "a403429f2ddf4b49b307e318f00e528b"}, req.body.input)
+    .then(data=> {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with api'))
+}
+const handleImage = (req, res, db) =>{
+    const {id} =req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+
+module.exports={
+    handleImage,
+    handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const { response } = require('express');
+
+const register = require('./controllers/register');
+const signIn = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image  = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'lukebeach',
+      password : '',
+      database : 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req, res) => {res.send(database.users)})
+
+// signin request
+app.post('/signin', (req, res) => {signIn.handleSignIn(req, res, db, bcrypt)})
+
+// register request
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+
+// /profile/:userID --> get = user
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)})
+
+// image
+app.put('/image',(req,res) => {image.handleImage(req, res, db)})
+
+//api
+app.post('/imageurl', (req, res) => {image.handleApiCall(req,res)})
+
+
+
+app.listen(3000, ()=>{
+    console.log('app is running on port 3000');
+})
